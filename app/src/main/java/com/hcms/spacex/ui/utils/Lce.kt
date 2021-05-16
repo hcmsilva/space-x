d181e69272e1f44388d772aa3dba689c1cd10bc2
hcmsilva <henriquecms@gmail.com>
2021-05-16 22:44:42 +0100

    added generic LCE for future improvement

---
+++ app/src/main/java/com/hcms/spacex/ui/utils/Lce.kt
@@ -0,0 +1,15 @@
+package com.hcms.spacex.ui.utils
+
+data class Lce<T> @JvmOverloads constructor(
+    val isLoading: Boolean = false,
+    val contentData: T? = null,
+    val error: Throwable? = null,
+    val hasError: Boolean = error != null,
+    val hasContentData: Boolean = contentData != null
+) {
+    companion object {
+        fun <T> content(content: T) = Lce<T>(contentData = content)
+        fun <T> loading() = Lce<T>(isLoading = true)
+        fun <T> error(t: Throwable) = Lce<T>(error = t)
+    }
+}
